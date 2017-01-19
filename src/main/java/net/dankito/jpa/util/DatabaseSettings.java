7355b6d96f34ba2bc4b79dca4ef390386becf353
dankito <dankito@user.noreply.github.com>
2017-01-19 23:28:27 +0100

    Using constants from Dao instead of strings

--- src/main/java/net/dankito/jpa/util/DatabaseSettings.java
+++ src/main/java/net/dankito/jpa/util/DatabaseSettings.java
@@ -21,10 +21,10 @@ public class DatabaseSettings {
 
   protected boolean isCouchbaseLiteSystemColumnName(String extractedColumnName) {
     switch(extractedColumnName) {
-      case "_id":
-      case "_revision":
-      case "attachments":
-      case "_deleted":
+      case Dao.ID_SYSTEM_COLUMN_NAME:
+      case Dao.REVISION_SYSTEM_COLUMN_NAME:
+      case Dao.ATTACHMENTS_SYSTEM_COLUMN_NAME:
+      case Dao.DELETED_SYSTEM_COLUMN_NAME:
       case Dao.TYPE_COLUMN_NAME: // yeah, actually not a Couchbase default 'Column', but i use it for Storing Entity Name // TODO: create a Constant for it
       default:
         return false;
